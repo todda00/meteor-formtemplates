aff1fae80ac57c3fdc32baec2861816e86080de8
todda00 <todd@toddhumphrey.com>
2015-01-22 12:10:52 -0500

    cleanup unneeded default code

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Provides form templates for a variety of bootstrap form elements',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/todda00/meteor-formtemplates.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -19,10 +19,4 @@ Package.onUse(function(api) {
     'formTemplates.html',
     'formTemplates.coffee'
   ],'client');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('todda00:formtemplates');
-  api.addFiles('todda00:formtemplates-tests.js');
 });
